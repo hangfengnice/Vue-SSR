61133171b42d0097188b1bf19c37bd5a270b5d15
hangfeng <nicehangfeng@foxmail.com>
2020-01-04 20:55:52 +0800

    jquery

--- gulpfile.js
+++ gulpfile.js
@@ -6,8 +6,8 @@ var del = require("del");
 var babel = require("gulp-babel");
 // var sass = require('gulp-sass');
 
-gulp.task("deldist", function() {
-  return del("dist");
+gulp.task("delbuild", function() {
+  return del("build");
 });
 
 // css
@@ -15,9 +15,9 @@ gulp.task("css", function() {
   return (
     gulp
       .src("./src/css/*.css")
-      .pipe(changed("dist", { extension: ".css" }))
+      .pipe(changed("build", { extension: ".css" }))
       // .pipe(sass()) //增加这行
-      .pipe(gulp.dest("./dist/css"))
+      .pipe(gulp.dest("./build/css"))
       .pipe(
         browserSync.reload({
           //内容更改则触发reload
@@ -27,18 +27,23 @@ gulp.task("css", function() {
   );
 });
 gulp.task("img", function() {
-  return gulp.src("./src/images").pipe(gulp.dest("./dist/images"));
+  return gulp.src("./src/images/*.{png,jpg,gif,jpeg,ico}").pipe(gulp.dest("./build/images")).pipe(
+    browserSync.reload({
+      //内容更改则触发reload
+      stream: true
+    })
+  );
 });
 gulp.task("js", function() {
   return gulp
     .src("./src/js/*.js")
-    .pipe(changed("dist"), { extension: ".js" })
-    .pipe(
-      babel({
-        presets: ["@babel/preset-env"]
-      })
-    )
-    .pipe(gulp.dest("dist/js"))
+    .pipe(changed("build"), { extension: ".js" })
+    // .pipe(
+    //   babel({
+    //     presets: ["@babel/preset-env"]
+    //   })
+    // )
+    .pipe(gulp.dest("build/js"))
     .pipe(
       browserSync.reload({
         //内容更改则触发reload
@@ -50,14 +55,14 @@ gulp.task("js", function() {
 gulp.task("html", function() {
   return gulp
     .src("./src/*.html")
-    .pipe(changed("dist"), { extension: ".html" })
+    .pipe(changed("build"), { extension: ".html" })
     .pipe(
       fileinclude({
-        basepath: "./src/common",
+        basepath: "./src/components",
         indent: true
       })
     )
-    .pipe(gulp.dest("dist"))
+    .pipe(gulp.dest("build"))
     .pipe(
       browserSync.reload({
         //内容更改则触发reload
@@ -66,21 +71,27 @@ gulp.task("html", function() {
     );
 });
 
+gulp.task("mp4", function() {
+  return gulp.src("./src/*.mp4")
+    .pipe(gulp.dest("build"))
+});
+
 gulp.task("watch", function() {
   // 建立浏览器自动刷新服务器
   browserSync.init({
     server: {
-      baseDir: "dist" // 设置服务器的根目录
+      baseDir: "build" // 设置服务器的根目录
     },
     notify: false //禁用浏览器的通知元素
   });
-
   gulp.watch("src/*.html", gulp.series("html"));
   gulp.watch("src/css/*.css", gulp.series("css"));
   gulp.watch("src/js/*.js", gulp.series("js"));
+  gulp.watch("./src/images/*.{png,jpg,gif,jpeg,ico}", gulp.series("img"));
+
 });
 
 gulp.task(
   "default",
-  gulp.series(gulp.parallel("deldist", "css", "img", "js", "html", "watch"))
+  gulp.series(gulp.parallel("delbuild", "css", "img", "js", "html","mp4", "watch"))
 );
